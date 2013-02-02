481a1c601a28c9e58b37e7ce7637ef7e5ffe273d
David Cramer <dcramer@gmail.com>
2013-02-02 14:06:56 -0800

    0.1.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ install_requires = [
 
 setup(
     name='django-static-compiler',
-    version='0.0.0',
+    version='0.1.0',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/dcramer/django-static-compiler',
